673f4391c34b6dd1bb9ef97a4bd2958ee7ce2cee
Dylan Little <dylan.c.little@gmail.com>
2022-03-20 20:58:32 -0600

    remove controllers from routes

---
+++ backend/controllers/shoeController.js
@@ -0,0 +1,59 @@
+const Shoe = require('../models/shoe');
+
+exports.getAllShoes = get('/', (req, res, next) => {
+  Shoe.find({}, (err, shoes) => {
+    if (err) {
+      res.status(500);
+      return next(err);
+    }
+    return res.status(200).send(shoes);
+  });
+});
+
+exports.createShoe = post('/:userId', (req, res, next) => {
+  const newShoe = new Shoe(req.body);
+  newShoe.save((err, savedShoe) => {
+    if (err) {
+      res.status(500);
+      return next(err);
+    }
+    User.updateOne(
+      { _id: req.params.userId },
+      { $addToSet: { shoes: savedShoe._id } },
+      (err) => {
+        if (err) {
+          res.status(500);
+          return next(err);
+        }
+        return res.status(200).send(savedShoe);
+      }
+    );
+  });
+});
+
+exports.updateShoe =
+  ('/:shoeId',
+  (req, res, next) => {
+    Shoe.findByIdAndUpdate(
+      { _id: req.params.shoeId },
+      req.body,
+      { new: true },
+      (err, updatedShoe) => {
+        if (err) {
+          res.status(500);
+          return next(500);
+        }
+        return res.status(200).send(updatedShoe);
+      }
+    );
+  });
+
+exports.deleteShoe = (req, res, next) => {
+  Shoe.findByIdAndDelete({ _id: req.params.shoeId }, (err, deletedShoe) => {
+    if (err) {
+      res.status(500);
+      return next(err);
+    }
+    return res.status(200).send(`successfully deleted ${deletedShoe.name}`);
+  });
+};
--- backend/routes/shoeRouter.js
+++ backend/routes/shoeRouter.js
@@ -1,65 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Shoe = require('../models/shoe');
-const User = require('../models/user');
 
-router.get('/', (req, res, next) => {
-  Shoe.find({}, (err, shoes) => {
-    if (err) {
-      res.status(500);
-      return next(err);
-    }
-    return res.status(200).send(shoes);
-  });
-});
+const shoeController = require('../controllers/shoeController');
+const { getAllShoes, createShoe, updateShoe, deleteShoe } = shoeController;
 
-router.post('/:userId', (req, res, next) => {
-  const newShoe = new Shoe(req.body);
-  newShoe.save((err, savedShoe) => {
-    if (err) {
-      res.status(500);
-      return next(err);
-    }
-    User.updateOne(
-      { _id: req.params.userId },
-      { $addToSet: { shoes: savedShoe._id } },
-      (err) => {
-        if (err) {
-          res.status(500);
-          return next(err);
-        }
-        return res.status(200).send(savedShoe);
-      }
-    );
-  });
-});
-
-router.put('/:shoeId', (req, res, next) => {
-  Shoe.findByIdAndUpdate(
-    { _id: req.params.shoeId },
-    req.body,
-    { new: true },
-    (err, updatedShoe) => {
-      if (err) {
-        res.status(500);
-        return next(500);
-      }
-      return res.status(200).send(updatedShoe);
-    }
-  );
-});
-
-const deleteShoe = (req, res, next) => {
-  Shoe.findByIdAndDelete({ _id: req.params.shoeId }, (err, deletedShoe) => {
-    if (err) {
-      res.status(500);
-      return next(err);
-    }
-    return res.status(200).send(`successfully deleted ${deletedShoe.name}`);
-  });
-};
-
-router.route('/').get().post();
-router.route('/:id').get().put().delete();
+router.route('/').get(getAllShoes).post(createShoe);
+router.route('/:id').put(updateShoe).delete(deleteShoe);
 
 module.exports = router;
