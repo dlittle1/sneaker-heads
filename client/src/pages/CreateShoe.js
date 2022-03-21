d89ec1a013e4b00cf6f090fdd98d8f38ae95973d
Dylan Little <dylan.c.little@gmail.com>
2022-03-20 22:40:03 -0600

    start css for new shoe form

--- client/src/pages/CreateShoe.js
+++ client/src/pages/CreateShoe.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../styles/createShoe.css';
 
 const CreateShoe = () => {
-  return <div>CreateShoe</div>;
+  return (
+    <div className='create-shoe-body'>
+      <div className='create-shoe-card'>
+        <h1 className='create-shoe-heading'>Add Your Sneaker</h1>
+      </div>
+    </div>
+  );
 };
 
 export default CreateShoe;
