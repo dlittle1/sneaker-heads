cd7f35f9c869ae16163473d301563181c720ab49
Dylan Little <dylan.c.little@gmail.com>
2022-03-22 19:36:28 -0600

    shoe page started

--- client/src/pages/Shoe.js
+++ client/src/pages/Shoe.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import '../styles/shoe.css';
 
 const Shoe = () => {
+  const params = useParams();
+  const shoeId = params.id;
+  const [shoe, setShoe] = useState({});
+
+  useEffect(() => {
+    const sendGetRequest = async () => {
+      try {
+        const res = await axios.get(`/shoes/${shoeId}`);
+        setShoe(res.data.shoe);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    sendGetRequest();
+  }, []);
+
   return <div>Shoe</div>;
 };
 
