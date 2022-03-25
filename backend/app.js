24fb840f30198c8e16709cafb88e6c157cc52b32
Dylan Little <dylan.c.little@gmail.com>
2022-03-24 23:16:23 -0600

    fix error handling middleware display client through express

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const shoeRouter = require('./routes/shoeRouter');
 const app = express();
 
 // MIDDLEWARE
-if (process.env.NODE_EVN === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
@@ -14,5 +14,17 @@ app.use(express.json());
 
 // ROUTES
 app.use('/api/shoes', shoeRouter);
+
+app.use((err, req, res, next) => {
+  return res.json({ errorMessage: err.message });
+});
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/build/index.html'));
+});
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,6 @@ const DB = process.env.DATABASE.replace(
 
 mongoose.connect(DB).then(() => console.log('connected to database'));
 
-app.use((err, res) => {
-  return res.json({ errorMessage: err.message });
-});
-
-const path = require('path');
-
-app.use(express.static(path.join('./../client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join('./../client/build/index.html'));
-});
-
 app.listen(process.env.PORT || 9000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
 });
