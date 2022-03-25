a95f015297d0572d8868e6a6cc6e572ed6c4d04d
Dylan Little <dylan.c.little@gmail.com>
2022-03-25 17:02:05 -0600

    fix button bug links to shoe page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -72,7 +72,50 @@ const Home = () => {
                 className='grid-img'
                 alt={`${shoe.name + ' ' + shoe.version}`}
               />
+              <div className='grid-item-buttons'>
+                <div
+                  className='grid-item-button-like grid-item-button'
+                  title='Add to Wishlist'
+                >
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    size='1x'
+                    className='grid-item-button-icon'
+                    style={{
+                      color: 'rgb(236, 122, 122)',
+                    }}
+                  />
+                </div>
 
+                <div
+                  className='grid-item-button grid-item-button-edit'
+                  title='Edit'
+                  onClick={() => handleEdit(shoe._id)}
+                >
+                  <FontAwesomeIcon
+                    icon={faPenToSquare}
+                    size='1x'
+                    className='grid-item-button-icon'
+                    style={{
+                      color: 'white',
+                    }}
+                  />
+                </div>
+                <div
+                  className='grid-item-button grid-item-button-delete'
+                  title='Delete'
+                  onClick={() => handleDelete(shoe._id)}
+                >
+                  <FontAwesomeIcon
+                    icon={faTrashCan}
+                    size='1x'
+                    className='grid-item-button-icon'
+                    style={{
+                      color: 'white',
+                    }}
+                  />
+                </div>
+              </div>
               <div
                 className='grid-text-container'
                 onClick={() => handleShoeClick(shoe._id)}
@@ -81,50 +124,6 @@ const Home = () => {
                   {shoe.name}
                 </h4>
                 <p className='grid-item-text'>{shoe.version}</p>
-                <div className='grid-item-buttons'>
-                  <div
-                    className='grid-item-button-like grid-item-button'
-                    title='Add to Wishlist'
-                  >
-                    <FontAwesomeIcon
-                      icon={faHeart}
-                      size='1x'
-                      className='grid-item-button-icon'
-                      style={{
-                        color: 'rgb(236, 122, 122)',
-                      }}
-                    />
-                  </div>
-
-                  <div
-                    className='grid-item-button grid-item-button-edit'
-                    title='Edit'
-                    onClick={() => handleEdit(shoe._id)}
-                  >
-                    <FontAwesomeIcon
-                      icon={faPenToSquare}
-                      size='1x'
-                      className='grid-item-button-icon'
-                      style={{
-                        color: 'white',
-                      }}
-                    />
-                  </div>
-                  <div
-                    className='grid-item-button grid-item-button-delete'
-                    title='Delete'
-                    onClick={() => handleDelete(shoe._id)}
-                  >
-                    <FontAwesomeIcon
-                      icon={faTrashCan}
-                      size='1x'
-                      className='grid-item-button-icon'
-                      style={{
-                        color: 'white',
-                      }}
-                    />
-                  </div>
-                </div>
               </div>
             </div>
           ))}
