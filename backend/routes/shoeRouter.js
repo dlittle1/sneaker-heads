6b9bc7f79b0b035f7dba232198641e255a3b2b7a
Dylan Little <dylan.c.little@gmail.com>
2022-04-12 00:00:11 -0600

    create comments on shoes

--- backend/routes/shoeRouter.js
+++ backend/routes/shoeRouter.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const shoeController = require('../controllers/shoeController');
-const { getAllShoes, getOneShoe, createShoe, updateShoe, deleteShoe } =
-  shoeController;
+const {
+  getAllShoes,
+  getOneShoe,
+  createShoe,
+  updateShoe,
+  deleteShoe,
+  createComment,
+} = shoeController;
 
 router.route('/').get(getAllShoes).post(createShoe);
 router.route('/:id').get(getOneShoe).put(updateShoe).delete(deleteShoe);
+router.route('/:id/comments').post(createComment);
 
 module.exports = router;
