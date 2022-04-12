effaff416fb50afd00408a25b636820ab141b1b7
Dylan Little <dylan.c.little@gmail.com>
2022-04-11 23:58:30 -0600

    allow for queries on get all shoes

--- backend/controllers/shoeController.js
+++ backend/controllers/shoeController.js
@@ -1,8 +1,36 @@
 const Shoe = require('../models/shoe');
 
 exports.getAllShoes = async (req, res, next) => {
+  let queryObject = { ...req.query };
+  const { page, limit, sort, ...query } = queryObject;
+
+  let queryString = JSON.stringify(queryObject);
+  queryString = queryString.replace(
+    /\b(gt|gte|lt|lte|in|regex)\b/g,
+    (match) => `$${match}`
+  );
+
   try {
-    const shoes = await Shoe.find({});
+    let query = Shoe.find(JSON.parse(queryString));
+    if (req.query.sort) {
+      let sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    }
+
+    const shoes = await query.populate([
+      {
+        path: 'user',
+        select: 'username',
+      },
+      {
+        path: 'comments',
+        populate: {
+          path: 'user',
+          select: 'username',
+        },
+      },
+    ]);
+
     return res.status(200).json(shoes);
   } catch (err) {
     return next(err);
@@ -64,6 +92,69 @@ exports.deleteShoe = async (req, res, next) => {
     });
   } catch (err) {
     res.status(500);
+    return next(err);
+  }
+};
+
+exports.createComment = async (req, res, next) => {
+  try {
+    const shoe = await Shoe.findById(req.params.id);
+    const comment = await Comment.create({
+      ...req.body,
+      user: req.user._id,
+    });
+    shoe.comments.push(comment._id);
+    await shoe.save();
+    return res.status(201).json(comment);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.likeShoe = async (req, res, next) => {
+  try {
+    const shoe = await Shoe.findById(req.params.id);
+    const user = await User.findById(req.user._id);
+    if (shoe.likes.includes(user._id)) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'You already liked this shoe!',
+      });
+    }
+    await Shoe.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $push: { likes: user._id } },
+      { new: true }
+    );
+    return res.status(200).json({
+      status: 'success',
+      message: 'You liked this shoe!',
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+exports.removeLike = async (req, res, next) => {
+  try {
+    const shoe = await Shoe.findById(req.params.id);
+    const user = await User.findById(req.user._id);
+    if (!shoe.likes.includes(user._id)) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'You have not liked this shoe!',
+      });
+    }
+    await Shoe.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $pull: { likes: user._id } },
+      { new: true }
+    );
+    return res.status(200).json({
+      status: 'success',
+      message: 'You unliked this shoe!',
+    });
+  } catch (err) {
     return next(err);
   }
 };
