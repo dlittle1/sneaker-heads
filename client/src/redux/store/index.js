8246047e07cabcf34202c186757206882835f0f3
Dylan Little <dylan.c.little@gmail.com>
2022-04-13 00:11:41 -0600

    add redux store to index

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
+import shoeReducer from '../features/shoeSlice';
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    shoes: shoeReducer,
   },
 });
