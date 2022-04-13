0e847bd9ae9414dc60fdfdc1b48f109bced6d10b
Dylan Little <dylan.c.little@gmail.com>
2022-04-13 00:08:29 -0600

    userLoggedIn routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ShoeForm from './pages/ShoeForm';
 import Shoe from './pages/Shoe';
 import LandingPage from './pages/LandingPage';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const user = useSelector((state) => state.user.isLoggedIn);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route element={<Navbar />}>
-          <Route path='/shoes' element={<Home />} />
+        <Route
+          path='/login'
+          element={user ? <Navigate replace to='/' /> : <LandingPage />}
+        />
+        <Route
+          path='/signup'
+          element={user ? <Navigate replace to='/' /> : <LandingPage />}
+        />
+        <Route element={!user ? <Navigate replace to='/login' /> : <Navbar />}>
+          <Route path='/' element={<Home />} />
           <Route path='/shoes/create' element={<ShoeForm />} />
           <Route path='/shoes/edit/:id' element={<ShoeForm />} />
           <Route path='/shoes/:id' element={<Shoe />} />
