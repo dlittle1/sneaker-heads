29434c62532ff48668155a4f895f902fa7caf72a
Dylan Little <dylan.c.little@gmail.com>
2022-04-15 00:15:15 -0600

    add timeago locale to app

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+
+TimeAgo.addDefaultLocale(en);
 
 ReactDOM.render(
   <Provider store={store}>
