23c2e5cca59fe59610e2f6680fbb89a5484a0cf3
Dylan Little <dylan.c.little@gmail.com>
2022-04-19 21:36:52 -0600

    refactor axios, to be better performant with custom method, create sort for shoes redux based

--- client/src/redux/features/shoeSlice.js
+++ client/src/redux/features/shoeSlice.js
@@ -1,33 +1,46 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import defaultAxios from 'axios';
 
-let userTokenAxios = axios.create({
-  baseURL: '/api/shoes',
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
-  },
-});
+const setAxiosHeaders = () => {
+  const token = localStorage.getItem('token');
+  let axios = defaultAxios.create({
+    baseURL: '/api/shoes',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-userTokenAxios.interceptors.response.use((response) => {
-  if (response.data.errorMessage === 'jwt expired') {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    window.location.reload();
-    return Promise.reject({
-      message: 'Your session has expired. Please log in again.',
-      response,
-    });
+  axios.interceptors.response.use((response) => {
+    if (response.data.errorMessage === 'jwt expired') {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      window.location.reload();
+      return Promise.reject({
+        message: 'Your session has expired. Please log in again.',
+        response,
+      });
+    }
+    return response;
+  });
+
+  return axios;
+};
+
+const useAxios = async (method, url, data) => {
+  try {
+    const axios = setAxiosHeaders();
+    const response = await axios[method](url, data);
+    return response;
+  } catch (error) {
+    return error.response;
   }
-  return response;
-});
+};
 
 export const getShoesAsync = createAsyncThunk(
   'shoes/getShoesAsync',
   async (payload, { rejectWithValue }) => {
     try {
-      const response = await userTokenAxios.get(`?sort=${payload}`, {
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-      });
+      const response = await useAxios('get', `?sort=${payload}`);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -39,7 +52,7 @@ export const getOneShoeAsync = createAsyncThunk(
   'shoes/getOneShoeAsync',
   async (payload, { rejectWithValue }) => {
     try {
-      const response = await userTokenAxios.get(`/${payload}`);
+      const response = await useAxios('get', `/${payload}`);
       return response.data.shoe;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -51,9 +64,7 @@ export const createShoeAsync = createAsyncThunk(
   'shoes/createShoeAsync',
   async (payload, { rejectWithValue }) => {
     try {
-      const response = await userTokenAxios.post('/', payload, {
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-      });
+      const response = await useAxios('post', '', payload);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -65,13 +76,9 @@ export const addCommentToShoeAsync = createAsyncThunk(
   'shoes/addCommentToShoeAsync',
   async (payload, { rejectWithValue }) => {
     try {
-      const response = await userTokenAxios.post(
-        `/${payload.shoeId}/comments`,
-        { comment: payload.body },
-        {
-          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-        }
-      );
+      const response = await useAxios('post', `/${payload.shoeId}/comments`, {
+        comment: payload.body,
+      });
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -83,9 +90,7 @@ export const likeShoe = createAsyncThunk(
   'shoes/likeShoe',
   async (payload, { rejectWithValue }) => {
     try {
-      const response = await userTokenAxios.post(`/${payload}/like`, {
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-      });
+      const response = await useAxios('post', `/${payload}/like`);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -97,7 +102,7 @@ export const updateShoeAsync = createAsyncThunk(
   'shoes/updateShoeAsync',
   async (payload, { rejectWithValue }) => {
     try {
-      const response = await userTokenAxios.put(`/${payload._id}`, payload);
+      const response = await useAxios('put', `/${payload.id}`, payload);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -109,7 +114,7 @@ export const deleteShoeAsync = createAsyncThunk(
   'shoes/deleteShoeAsync',
   async (payload, { rejectWithValue }) => {
     try {
-      const response = await userTokenAxios.delete(`/${payload}`);
+      const response = await useAxios('delete', `/${payload}`);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -122,6 +127,7 @@ export const shoeSlice = createSlice({
   initialState: {
     shoes: [],
     shoe: {},
+    sort: 'numLikes',
   },
   reducers: {
     setShoes: (state, action) => {
@@ -129,6 +135,18 @@ export const shoeSlice = createSlice({
     },
     setShoe: (state, action) => {
       state.shoe = action.payload;
+    },
+    setSort: (state, action) => {
+      state.sort = action.payload;
+      state.shoes.sort((a, b) => {
+        if (a[state.sort] < b[state.sort]) {
+          return 1;
+        }
+        if (a[state.sort] > b[state.sort]) {
+          return -1;
+        }
+        return 0;
+      });
     },
   },
   extraReducers: {
@@ -173,6 +191,6 @@ export const shoeSlice = createSlice({
   },
 });
 
-export const { setShoe } = shoeSlice.actions;
+export const { setShoe, setSort } = shoeSlice.actions;
 
 export default shoeSlice.reducer;
