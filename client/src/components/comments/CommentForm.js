cf7b7d3da991c92b44fb6b40c892f049685cf4b9
Dylan Little <dylan.c.little@gmail.com>
2022-04-24 21:13:17 -0600

    move commentform to comments components folder

--- client/src/components/comments/CommentForm.js
+++ client/src/components/comments/CommentForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addCommentToShoeAsync } from '../redux/features/shoeSlice';
+import { addCommentToShoeAsync } from '../../redux/features/shoeSlice';
 
 const CommentForm = ({
   isCommenting,
